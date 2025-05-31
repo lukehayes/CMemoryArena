c276e8057f0f9dd5ab9ca9d6da78e55de9900ad8
lukehayes <lukepurplehayes@gmail.com>
2025-05-31 09:54:20 +0100

    fix: add missing '=' to for loop

--- src/Arena.c
+++ src/Arena.c
@@ -74,7 +74,7 @@ void ArenaPrint(Arena* arena)
     printf("####################################\n");
     printf("");
 
-    for (int i 0; i <= arena->capacity; i++)
+    for (int i = 0; i <= arena->capacity; i++)
     {
         if (arena->values[i]) {
             printf("[1]");
