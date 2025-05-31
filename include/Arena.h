0fdaa13bec3104c248e132d269b3d9cc279487c8
lukehayes <lukepurplehayes@gmail.com>
2025-05-31 09:52:06 +0100

    docs: add missing doc blocks

--- include/Arena.h
+++ include/Arena.h
@@ -7,18 +7,48 @@
 
 typedef struct Arena Arena;
 
+/**
+* Create a new  memory arena.
+*
+* @param size_t bufferSize.
+*/
 Arena* ArenaCreate(size_t bufferSize);
 
-void ArenaInsert(Arena* arena, size_t val);
+/**
+* Insert data into the arena at the next available slot.
+*
+* @param Arena* arena.
+* @param size_t data.
+*/
+void ArenaInsert(Arena* arena, size_t data);
 
-void ArenaInsertAt(Arena* arena, size_t pos, size_t val);
+/**
+* Insert data into the arena at the a specific location.
+*
+* @param Arena* arena.
+* @param size_t data.
+*/
+void ArenaInsertAt(Arena* arena, size_t pos, size_t data);
 
+/**
+* Set all of the memory of the arena to zero.
+*/
 void ArenaClear(Arena* arena);
 
 size_t* ArenaGetValues(Arena* arena);
 
+/**
+* Free all memory associated with this arena.
+*
+* @param Arena* arena.
+*/
 void ArenaDestroy(Arena* arena);
 
+/**
+* Print the arena to stdout.
+*
+* @param Arena* arena.
+*/
 void ArenaPrint(Arena* arena);
 
 #endif // !LDH_MEM_ARENA_H
